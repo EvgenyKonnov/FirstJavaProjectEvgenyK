cb477accdfba497025e5f1176b8f554d096d4efb
Lenovo <eu.konnov@yandex.ru>
2026-03-04 22:03:35 +0300

    Добавил step в автотест

--- src/test/java/TestPet.java
+++ src/test/java/TestPet.java
@@ -1,12 +1,10 @@
-import io.qameta.allure.Description;
-import io.qameta.allure.Owner;
-import io.qameta.allure.Severity;
-import io.qameta.allure.SeverityLevel;
+import io.qameta.allure.*;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
+import static io.qameta.allure.Allure.step;
 import static io.restassured.RestAssured.given;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -14,24 +12,30 @@ public class TestPet {
 
     private static String BASE_URL = "http://5.181.109.28:9090/api/v3";
 
+    @Test
     @Owner("Evgeny Konnov")
     @Description("Проверка удаления несуществующего питомца")
     @Severity(SeverityLevel.CRITICAL)
-    @Tag("api")
-    @Test
+    @Feature("Pet")
     public void testDeleteNoneExistentPet() {
-        Response response = given()
+        Response response = step("Проверить что статус-код в ответе == 200", ()->
+            given()
                 .contentType(ContentType.JSON)
                 .header("Accept", "application/json")
                 .when()
-                .delete(BASE_URL + "/pet/9999");
+                .delete(BASE_URL + "/pet/9999")
+        );
 
         String responseBody = response.getBody().asString();
 
-        assertEquals(200, response.getStatusCode(),
-                "Код ответа не совпал с ожидаемым. Ответ: " + responseBody);
+        step("Проверить что статус-код в ответе == 200", ()->
+                assertEquals(200, response.getStatusCode(),
+                        "Код ответа не совпал с ожидаемым. Ответ: " + responseBody)
+                );
 
-        assertEquals("Pet deleted", responseBody,
-                "Текст ошибки не совпал с ожидаемым. Получен: " + responseBody);
+        step("Проверить что текст в ответе 'Pet deleted'", ()->
+                        assertEquals("Pet deleted", responseBody,
+                                "Текст ошибки не совпал с ожидаемым. Получен: " + responseBody)
+                );
     }
 }
