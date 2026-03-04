2151146853940e69ce0eac77a18e96e39a6c44d8
Lenovo <eu.konnov@yandex.ru>
2026-03-04 21:19:32 +0300

    Добавил параметры Allure

--- src/test/java/TestPet.java
+++ src/test/java/TestPet.java
@@ -1,5 +1,10 @@
+import io.qameta.allure.Description;
+import io.qameta.allure.Owner;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
@@ -9,6 +14,10 @@ public class TestPet {
 
     private static String BASE_URL = "http://5.181.109.28:9090/api/v3";
 
+    @Owner("Evgeny Konnov")
+    @Description("Проверка удаления несуществующего питомца")
+    @Severity(SeverityLevel.CRITICAL)
+    @Tag("api")
     @Test
     public void testDeleteNoneExistentPet() {
         Response response = given()
